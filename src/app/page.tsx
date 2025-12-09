08bc7306b90a16625c6707bc7cbd370aa3c34d6a
Karan Mehta <mehtakaran8872@gmail.com>
2025-12-10 01:40:10 +0530

    Remove Accreditations component from Home
    Removed Accreditations component from Home page.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,11 @@
 import { Hero } from "@/components/home/Hero";
 import { ServicesSection } from "@/components/home/ServicesSection";
-import { Accreditations } from "@/components/home/Accreditations";
 
 export default function Home() {
   return (
     <>
       <Hero />
       <ServicesSection />
-      <Accreditations />
     </>
   );
 }
